326254fc424e20829b99f346f8214a7301329be3
Annie Luc <lannie3186@gmail.com>
2019-07-23 13:09:45 -0700

    radio button are seperated by each question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,15 @@
 
 
-  
+
 //Timer starting at 50 seconds and decrementing to 0
 var counter = 5
-$("#counter").text("Time Remaining: " + counter);
+$("#counterValue").text(counter);
 
-var timer = setInterval(goDown, 1*1000);
+var timer = setInterval(goDown, 1000);
 
-function goDown(){
+function goDown() {
     counter--;
-    $("#counter").text("Time Remaining: " + counter);
+    $("#counterValue").text(counter);
 
     if (counter == 0) {
         $("input[type='submit']").attr("disabled", true);
@@ -17,10 +17,10 @@ function goDown(){
         $("#content").text("Times Up!!")
     }
 }
-        // var ans = $("input[name='borders']:checked").val();
-
-		// if (ans == 'Oregan'){
-		// 	$('#content').text('correct');
-		// }else{
-		// 	$('#content').text('incorrect')
-		// }
+var ans = $("input[name='borders']:checked").val();
+console.log(ans)
+if (ans == 'Oregon') {
+    $('#content').text('correct');
+} else {
+    $('#content').text('incorrect')
+}
