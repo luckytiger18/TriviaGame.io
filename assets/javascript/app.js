9babb685203b0fd2f323c2534a5f551f4f28cfc1
Annie Luc <lannie3186@gmail.com>
2019-07-23 23:37:38 -0700

    added endGame, added evaluateGame

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,7 @@
-var counter;
+var counter = 120;
 var timer;
-var playerCorrectAnswers;
-var playerIncorrectAnswers;
-
-$(document).ready(function () {
-    $("#quiz").hide();
-});
-
-$("#startButton").on("click", function () {
-    counter = 20;
-    timer = setInterval(goDown, 1000);
-    $("#startButton").remove();
-    $("#quiz").show();
-});
+var playerCorrectAnswers = 0;
+var playerIncorrectAnswers = 0;
 
 function goDown() {
     counter--;
@@ -21,41 +10,88 @@ function goDown() {
     if (counter == 0) {
         $("input[type='submit']").attr("disabled", true);
         clearInterval(timer);
-        $("#content").text("Times Up!!")
+        $("#content").text("Times Up!!");
+        evaluateGame();
+        endGame();
     }
 }
-//Radio Button 
-$("input[type='submit']").on('click', function () {
+function endGame() {
+    $("#quiz").hide();
+    clearInterval(timer);
+    $("#correctResults").text("Correct: " + playerCorrectAnswers)
+    $("#incorrectResults").text("Incorrect: " + playerIncorrectAnswers)
+    $("#results").show();
+    $("#counter").hide();
+    $("#counterValue").hide();
+}
+
+function evaluateGame() {
     var ansOne = $("input[name='borders']:checked").val();
     if (ansOne == "oregon") {
         console.log("correct")
+        playerCorrectAnswers++;
     } else {
+        playerIncorrectAnswers++;
         console.log("incorrect")
     }
     var ansTwo = $("input[name='centralValley']:checked").val();
     if (ansTwo == "centralValley") {
         console.log("correct")
+        playerCorrectAnswers++;
     } else {
         console.log("incorrect")
+        playerIncorrectAnswers++;
     }
     var ansThree = $("input[name='capital']:checked").val();
     if (ansThree == "sacramento") {
         console.log("correct")
+        playerCorrectAnswers++;
     } else {
         console.log("incorrect")
+        playerIncorrectAnswers++;
     }
     var ansFour = $("input[name='stateFlower']:checked").val();
     if (ansFour == "goldenPoppy") {
         console.log("correct")
+        playerCorrectAnswers++;
     } else {
         console.log("incorrect")
+        playerIncorrectAnswers++;
     }
     var ansFive = $("input[name='goldenState']:checked").val();
     if (ansFive == "golden") {
         console.log("correct")
+        playerCorrectAnswers++;
     } else {
         console.log("incorrect")
+        playerIncorrectAnswers++;
     }
+}
+$(document).ready(function () {
+    $("#quiz").hide();
+    $("#results").hide();
+    $("#counter").hide();
+    $("#counterValue").hide();
+});
+
+$("#startButton").on("click", function () {
+    goDown();
+    timer = setInterval(goDown, 1000);
+    $("#startButton").remove();
+    $("#quiz").show();
+    $("#counter").show();
+    $("#counterValue").show();
+});
+
+//Radio Button 
+$("#finishQuiz").on('click', function () {
+    evaluateGame();
+
+    console.log(playerCorrectAnswers)
+    console.log(playerIncorrectAnswers)
+    endGame();
+
     event.preventDefault();
 });
-    
+
+
